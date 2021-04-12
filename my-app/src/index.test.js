960f9a67243b453b416fc6e911a661e2c261370e
Shaahin S <shaahin@gmail.com>
2021-04-12 00:56:06 -0500

    a bit more test scenarios

--- my-app/src/index.test.js
+++ my-app/src/index.test.js
@@ -4,10 +4,15 @@ import { unmountComponentAtNode } from "react-dom";
 import Game from './index';
 
 let container = null;
+let sqElement;
+
+// Helper vid: https://www.youtube.com/watch?v=3e1GHCA3GP0
+
 beforeEach(() => {
   // setup a DOM element as a render target
   container = document.createElement("div");
   document.body.appendChild(container);
+  render(<Game />, container);  
 });
 
 afterEach(() => {
@@ -17,8 +22,19 @@ afterEach(() => {
   container = null;
 });
 
-test('render tic toc board', () => {
-  render(<Game />, container);
-  const sqElement = screen.getByTestId(/aa0/i);
+test('Render tic toc board', () => {
+  // render(<Game />, container);
+
+  sqElement = screen.getByTestId(/aa0/i);
   expect(sqElement).toBeInTheDocument();
+
+});
+
+test('Check board functionality', () => {
+  sqElement = screen.getByTestId(/aa0/i);
+  sqElement.click();
+  expect(sqElement).toHaveTextContent("X");
+  sqElement = screen.getByTestId(/aa1/i);
+  sqElement.click();
+  expect(sqElement).toHaveTextContent("O");
 });
