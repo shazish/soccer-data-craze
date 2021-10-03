eb1d10ee2a2c2cbf57eec7f68fd7b76bf925ad12
Shaun <shaahin@gmail.com>
2021-10-03 01:23:12 -0500

    init store and react-redux provider

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
+import configureStore from "./redux/configureStore";
+import { Provider as ReduxProvider } from "react-redux";
 
+const store = configureStore();
 // https://reactjs.org/tutorial/tutorial.html#setup-for-the-tutorial
 // npm start
 
@@ -13,8 +16,10 @@ ReactDOM.render(
   // <Provider store="this.props.store">
 
   // </Provider>
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById("root")
+  <ReduxProvider store={store}>
+    <Router>
+      <App />
+    </Router>
+    , document.getElementById("root")
+  </ReduxProvider>
 );
