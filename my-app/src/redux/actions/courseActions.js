b1284b87953ce444281e9e9c0816bae625569dea
Shaun <65170032+shaun-shahbazi-colpal@users.noreply.github.com>
2021-10-13 23:03:41 -0500

    add reducer

--- my-app/src/redux/actions/courseActions.js
+++ my-app/src/redux/actions/courseActions.js
@@ -1,5 +1,25 @@
 import * as types from "./actionTypes";
+import * as coursesApi from "../../api/courseApi";
 
 export function createCourse(course) {
   return { type: types.CREATE_COURSE, course };
+}
+
+export function loadCoursesSuccess(courses) {
+  return { type: types.LOAD_COURSES_SUCCESS, courses };
+}
+
+// thunk: this signature adds dispatch as arg.
+// we no longer have to pass dispatch ourselves
+export function loadCourses() {
+  return function (dispatch) {
+    return coursesApi
+      .getCourses()
+      .then((courses) => {
+        dispatch(loadCoursesSuccess(courses));
+      })
+      .catch((err) => {
+        throw err;
+      });
+  };
 }
